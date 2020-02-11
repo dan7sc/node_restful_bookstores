320916f680e044c94b46ae904d17130660d7882e
daniel sc <su7cheng@gmail.com>
2020-02-11 15:42:34 -0200

    Refactor dao

--- src/dao/bookstore.js
+++ src/dao/bookstore.js
@@ -2,16 +2,8 @@ const db = {};
 
 export default class BookstoreDAO {
     static async injectDB(conn) {
-        // if (bookstores) {
-        //     return;
-        // }
         try {
             db.conn = await conn;
-            // const Bookstore = await db.conn.models.Bookstore;
-            // const bookstore = await Bookstore.findOne({})
-            //       .then(result => result.get())
-            //       .catch(error => console.error(error));
-            // console.log(bookstore);
         }
         catch (error) {
             console.error(`Unable to establish a connection: ${error}`);
@@ -21,9 +13,7 @@ export default class BookstoreDAO {
     static async getBookstores() {
         try {
             const Bookstore = db.conn.models.Bookstore;
-            const bookstores = await Bookstore.findAll({})
-                  .then(result => result)
-                  .catch(error => console.error(error));
+            const bookstores = await Bookstore.findAll({});
             return bookstores;
         }
         catch (error) {
