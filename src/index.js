00078907f5945168e1c77d86bfeae05b97d2d199
dan7sc <su7cheng@gmail.com>
2020-02-13 16:06:03 -0300

    Remove sequelize logging

--- src/index.js
+++ src/index.js
@@ -21,8 +21,9 @@ const sequelize = new Sequelize(
             min: 0,
             acquire: 30000,
             idle: 10000
-        }
-    }
+        },
+        logging: false
+    },
 );
 
 db.sequelize = sequelize;
