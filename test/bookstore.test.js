b529ed96da12b8bb8076401cc6ea886d8eff0f0f
dan7sc <su7cheng@gmail.com>
2020-02-15 13:46:18 -0300

    Refactor bookstore tests

--- test/bookstore.test.js
+++ test/bookstore.test.js
@@ -1,62 +1,70 @@
 import BookstoreDAO from '../src/dao/bookstore';
-describe('GET method', () => {
+describe('bookstore routes', () => {
     beforeAll(async () => {
         await BookstoreDAO.injectDB(global.appClient);
     });
 
-    test('should get bookstores', async () => {
-        const bookstores = await BookstoreDAO.getBookstores();
-        const firstBookstore = bookstores[0];
-        const lastBookstore  = bookstores[bookstores.length-1];
-        expect(firstBookstore.name).toEqual('Urban Fantasy');
-        expect(lastBookstore.name).toEqual('Updated!');
-        expect(bookstores.length).toBe(5);
+    describe('GET method', () => {
+        test('should get bookstores', async () => {
+            const bookstores = await BookstoreDAO.getBookstores();
+            const firstBookstore = bookstores[0];
+            const lastBookstore  = bookstores[bookstores.length-1];
+            expect(firstBookstore.name).toEqual('Urban Fantasy');
+            expect(lastBookstore.name).toEqual('Updated!');
+            expect(bookstores.length).toBe(5);
+        });
+
+        test('should get a bookstore', async () => {
+            const ids = [
+                "6bd895ce-af7a-451a-8b25-50c2876e162a",
+                "6bd895ce-af7a-451a-8b25-50c2876e162e"
+            ];
+            const firstBookstore = await BookstoreDAO.getBookstoreById(ids[0]);
+            const lastBookstore = await BookstoreDAO.getBookstoreById(ids[1]);
+            expect(firstBookstore.name).toEqual('Urban Fantasy');
+            expect(lastBookstore.name).toEqual('Updated!');
+            expect(firstBookstore.length).toBe(undefined);
+            expect(lastBookstore.length).toBe(undefined);
+        });
     });
 
-    test('should get a bookstore', async () => {
-        const ids = [
-            "6bd895ce-af7a-451a-8b25-50c2876e162a",
-            "6bd895ce-af7a-451a-8b25-50c2876e162e"
-        ];
-        const firstBookstore = await BookstoreDAO.getBookstoreById(ids[0]);
-        const lastBookstore = await BookstoreDAO.getBookstoreById(ids[1]);
-        expect(firstBookstore.name).toEqual('Urban Fantasy');
-        expect(lastBookstore.name).toEqual('Updated!');
-        expect(firstBookstore.length).toBe(undefined);
+    describe('POST method', () => {
+        test('should add a bookstore', async () => {
+            const bookstoreToAdd = {
+                id: '6bd895ce-af7a-451a-8b25-50c2876e162f',
+                name: 'myBookstore',
+                picture: 'my_bookstore_image.png',
+                customerId: '9f933f19-d3c6-4fa1-a161-0a2a052fdc65'
+            };
+            const addedBookstore = await BookstoreDAO.addBookstore(bookstoreToAdd);
+            expect(addedBookstore.name).toEqual(bookstoreToAdd.name);
+            expect(addedBookstore.picture).toEqual(bookstoreToAdd.picture);
+        });
     });
 
-    test('should add a bookstore', async () => {
-        const bookstoreToAdd = {
-            id: '6bd895ce-af7a-451a-8b25-50c2876e162f',
-            name: 'myBookstore',
-            picture: 'my_bookstore_image.png',
-            customerId: '9f933f19-d3c6-4fa1-a161-0a2a052fdc65'
-        };
-        const addedBookstore = await BookstoreDAO.addBookstore(bookstoreToAdd);
-        const response = await BookstoreDAO.getBookstoreById(addedBookstore.id);
-        expect(addedBookstore.name).toEqual(response.name);
-        expect(addedBookstore.picture).toEqual(response.picture);
+    describe('PUT method', () => {
+        test('should update bookstore', async () => {
+            const id = '6bd895ce-af7a-451a-8b25-50c2876e162f';
+            const dataToUpdate = {
+                name: 'myUpdatedBookstore',
+                picture: 'updated_image.png'
+            };
+            const numberOfUpdatedBookstores = await BookstoreDAO.updateBookstore(id, dataToUpdate);
+            const updatedBookstore = await BookstoreDAO.getBookstoreById(id);
+            expect(numberOfUpdatedBookstores.pop()).toBe(1);
+            expect(updatedBookstore.id).toEqual(id);
+            expect(updatedBookstore.name).toEqual(dataToUpdate.name);
+            expect(updatedBookstore.picture).toEqual(dataToUpdate.picture);
+        });
     });
 
-    test('should update bookstore', async () => {
-        const id = '6bd895ce-af7a-451a-8b25-50c2876e162f';
-        const dataToUpdate = {
-            name: 'myUpdatedBookstore',
-            picture: 'updated_image.png'
-        };
-        const response = await BookstoreDAO.updateBookstore(id, dataToUpdate);
-        const updatedBookstore = await BookstoreDAO.getBookstoreById(id);
-        expect(response.pop()).toBe([1].pop());
-        expect(updatedBookstore.id).toEqual(id);
-        expect(updatedBookstore.name).toEqual(dataToUpdate.name);
-        expect(updatedBookstore.picture).toEqual(dataToUpdate.picture);
-    });
-
-    test('should delete bookstore', async () => {
-        const id = '6bd895ce-af7a-451a-8b25-50c2876e162f';
-        const response = await BookstoreDAO.deleteBookstore(id);
-        const bookstore = await BookstoreDAO.getBookstoreById(id);
-        expect(response).toBe(1);
-        expect(bookstore).toEqual(null);
+    describe('DELETE method', () => {
+        test('should delete bookstore', async () => {
+            const id = '6bd895ce-af7a-451a-8b25-50c2876e162f';
+            const numberOfDeletedBookstores = await BookstoreDAO.deleteBookstore(id);
+            const bookstore = await BookstoreDAO.getBookstoreById(id);
+            expect(numberOfDeletedBookstores).toBe(1);
+            expect(bookstore).toEqual(null);
+        });
     });
 });
