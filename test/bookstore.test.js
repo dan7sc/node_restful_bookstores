0f881320cb25870f65e74325a5e9dbeb590a8199
dan7sc <su7cheng@gmail.com>
2020-02-15 12:10:32 -0300

    Test should get a bookstore

--- test/bookstore.test.js
+++ test/bookstore.test.js
@@ -1,5 +1,4 @@
 import BookstoreDAO from '../src/dao/bookstore';
-
 describe('GET method', () => {
     beforeAll(async () => {
         await BookstoreDAO.injectDB(global.appClient);
@@ -12,5 +11,17 @@ describe('GET method', () => {
         expect(firstBookstore.name).toEqual('Urban Fantasy');
         expect(lastBookstore.name).toEqual('Updated!');
         expect(bookstores.length).toBe(5);
+    });
+
+    test('should get a bookstore', async () => {
+        const ids = [
+            "6bd895ce-af7a-451a-8b25-50c2876e162a",
+            "6bd895ce-af7a-451a-8b25-50c2876e162e"
+        ];
+        const firstBookstore = await BookstoreDAO.getBookstoreById(ids[0]);
+        const lastBookstore = await BookstoreDAO.getBookstoreById(ids[1]);
+        expect(firstBookstore.name).toEqual('Urban Fantasy');
+        expect(lastBookstore.name).toEqual('Updated!');
+        expect(firstBookstore.length).toBe(undefined);
     });
 });
