a77ea022fa3541f91b586a24af04e2e51392040e
dan7sc <su7cheng@gmail.com>
2020-02-17 10:59:29 -0300

    Test should get a book

--- test/book-dao.test.js
+++ test/book-dao.test.js
@@ -18,17 +18,32 @@ describe('book dao', () => {
 
     test('should add a book', async () => {
         const bookToAdd = {
-            "id": "ba48ba34-6609-4468-aa5e-2b7b479d6053",
-            "title": "The Name of the Rose",
-            "author": "Umberto Eco",
-            "genre": "Historical Mystery",
-            "description": "A novel.",
-            "price": "22.29",
-            "bookstoreId": "6bd895ce-af7a-451a-8b25-50c2876e162e"
+            'id': 'ba48ba34-6609-4468-aa5e-2b7b479d6053',
+            'title': 'The Name of the Rose',
+            'author': 'Umberto Eco',
+            'genre': 'Historical Mystery',
+            'description': 'A novel.',
+            'price': '22.29',
+            'bookstoreId': '6bd895ce-af7a-451a-8b25-50c2876e162e'
         };
         const addedBook = await BookDAO.addBook(bookToAdd);
         expect(addedBook.id).toEqual(bookToAdd.id);
         expect(addedBook.title).toEqual(bookToAdd.title);
         expect(addedBook.bookstoreId).toEqual(bookToAdd.bookstoreId);
+    });
+
+    test('should get a book', async () => {
+        const id = [
+            'ba48ba34-6609-4468-aa5e-2b7b479d6040',
+            'ba48ba34-6609-4468-aa5e-2b7b479d6051'
+        ];
+        const firstBook = await BookDAO.getBookById(id[0]);
+        const lastBook = await BookDAO.getBookById(id[1]);
+        expect(firstBook.id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6040');
+        expect(firstBook.title).toEqual('The Hobbit');
+        expect(lastBook.length).toBe(undefined);
+        expect(lastBook.id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6051');
+        expect(lastBook.title).toEqual('Atonement');
+        expect(lastBook.length).toBe(undefined);
     });
 });
