3939144766305c04ae8fcb24a4b2ce96acffd422
dan7sc <su7cheng@gmail.com>
2020-02-17 10:29:09 -0300

    Test should add a book

--- test/book-dao.test.js
+++ test/book-dao.test.js
@@ -5,7 +5,7 @@ describe('book dao', () => {
         await BookDAO.injectDB(global.appClient);
     });
 
-    test('get books', async () => {
+    test('should get books', async () => {
         const books = await BookDAO.getBooks();
         const firstBook = books[0];
         const lastBook = books[books.length-1];
@@ -14,5 +14,21 @@ describe('book dao', () => {
         expect(lastBook.id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6051');
         expect(lastBook.title).toEqual('Atonement');
         expect(books.length).toBe(12);
+    });
+
+    test('should add a book', async () => {
+        const bookToAdd = {
+            "id": "ba48ba34-6609-4468-aa5e-2b7b479d6053",
+            "title": "The Name of the Rose",
+            "author": "Umberto Eco",
+            "genre": "Historical Mystery",
+            "description": "A novel.",
+            "price": "22.29",
+            "bookstoreId": "6bd895ce-af7a-451a-8b25-50c2876e162e"
+        };
+        const addedBook = await BookDAO.addBook(bookToAdd);
+        expect(addedBook.id).toEqual(bookToAdd.id);
+        expect(addedBook.title).toEqual(bookToAdd.title);
+        expect(addedBook.bookstoreId).toEqual(bookToAdd.bookstoreId);
     });
 });
