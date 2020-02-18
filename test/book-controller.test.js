95228bc582973e18e602e51567c8a916b15f5dd1
dan7sc <su7cheng@gmail.com>
2020-02-18 12:52:56 -0300

    Test book controller get a book from a bookstore

--- test/book-controller.test.js
+++ test/book-controller.test.js
@@ -2,8 +2,13 @@ import BookDAO from '../src/dao/book';
 import BookCtrl from '../src/controllers/book';
 
 jest.mock('express');
-const mockRequest = (id) => {
-    const req = { params: { bookstoreId: id } };
+const mockRequest = (bookstoreId, bookId) => {
+    const req = {
+        params: {
+            bookstoreId: bookstoreId,
+            bookId: bookId
+        }
+    };
     return req;
 };
 const mockResponse = () => {
@@ -31,7 +36,7 @@ describe('book ctrl', () => {
     test('should get books from a bookstore', async () => {
         const id = '6bd895ce-af7a-451a-8b25-50c2876e162a';
         const res = mockResponse();
-        const req = mockRequest(id);
+        const req = mockRequest(id, null);
         const books = await BookCtrl.apiGetBooksByBookstoreId(req, res);
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.setHeader).toHaveBeenCalledWith("Content-Type", "application/json");
@@ -40,5 +45,18 @@ describe('book ctrl', () => {
         expect(books[3].id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6043');
         expect(books[3].bookstoreId).toEqual('6bd895ce-af7a-451a-8b25-50c2876e162a');
         expect(books.length).toBe(4);
+    });
+
+    test('should get a book from a bookstore', async () => {
+        const bookstoreId = '6bd895ce-af7a-451a-8b25-50c2876e162a';
+        const bookId = 'ba48ba34-6609-4468-aa5e-2b7b479d6040';
+        const res = mockResponse();
+        const req = mockRequest(bookstoreId, bookId);
+        const book = await BookCtrl.apiGetBookByBookstoreId(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.setHeader).toHaveBeenCalledWith("Content-Type", "application/json");
+        expect(book.id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6040');
+        expect(book.bookstoreId).toEqual('6bd895ce-af7a-451a-8b25-50c2876e162a');
+        expect(book.length).toBe(undefined);
     });
 });
