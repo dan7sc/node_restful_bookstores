6a439cdce98a7eb16e03385baed02aa7d65eb49b
dan7sc <su7cheng@gmail.com>
2020-02-18 12:37:30 -0300

    Test book controller get books from a bookstore

--- test/book-controller.test.js
+++ test/book-controller.test.js
@@ -2,6 +2,10 @@ import BookDAO from '../src/dao/book';
 import BookCtrl from '../src/controllers/book';
 
 jest.mock('express');
+const mockRequest = (id) => {
+    const req = { params: { bookstoreId: id } };
+    return req;
+};
 const mockResponse = () => {
     const res = {};
     res.status = jest.fn().mockReturnValue(res);
@@ -22,5 +26,19 @@ describe('book ctrl', () => {
         expect(res.setHeader).toHaveBeenCalledWith("Content-Type", "application/json");
         expect(books[0].id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6040');
         expect(books[11].id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6051');
+    });
+
+    test('should get books from a bookstore', async () => {
+        const id = '6bd895ce-af7a-451a-8b25-50c2876e162a';
+        const res = mockResponse();
+        const req = mockRequest(id);
+        const books = await BookCtrl.apiGetBooksByBookstoreId(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.setHeader).toHaveBeenCalledWith("Content-Type", "application/json");
+        expect(books[0].id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6040');
+        expect(books[0].bookstoreId).toEqual('6bd895ce-af7a-451a-8b25-50c2876e162a');
+        expect(books[3].id).toEqual('ba48ba34-6609-4468-aa5e-2b7b479d6043');
+        expect(books[3].bookstoreId).toEqual('6bd895ce-af7a-451a-8b25-50c2876e162a');
+        expect(books.length).toBe(4);
     });
 });
