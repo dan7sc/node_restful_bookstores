bf0ce3fee7fc573e0bf227da80d77de34023caca
dan7sc <su7cheng@gmail.com>
2020-02-21 12:55:53 -0300

    Add customer dao

---
+++ src/dao/customer.js
@@ -0,0 +1,44 @@
+let Customer;
+
+export default class CustomerDAO {
+    static async injectDB(conn) {
+        try {
+            Customer = conn.models.Customer;
+        }
+        catch(error) {
+            console.error(`Unable to establish a connection: ${error}`);
+        }
+    }
+
+    static async addCustomer(data) {
+        try {
+            const newCustomer = await Customer.create(data);
+            return newCustomer;
+        } catch(error) {
+            console.log(`Could not add customer: ${error}`);
+            return error;
+        }
+    }
+
+    static async getCustomerById(id) {
+        try {
+            const options = { id: id };
+            const customer = await Customer.findOne({ where: options });
+            return customer;
+        } catch(error) {
+            console.log(`Could not get customer with id ${id}: ${error}`);
+            return error;
+        }
+    }
+
+    static async deleteCustomer(id) {
+        try {
+            const options = { id: id };
+            const deletedCustomer = await Customer.destroy({ where: options });
+            return deletedCustomer;
+        } catch(error) {
+            console.log(`Could not delete customer: ${error}`);
+            return error;
+        }
+    }
+ }
