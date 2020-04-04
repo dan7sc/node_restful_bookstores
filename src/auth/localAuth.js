1f95fddf3408cb26781c1fb31f76be64f0faa7dd
dan7sc <su7cheng@gmail.com>
2020-04-04 15:18:58 -0300

    Remove serializeUser

--- src/auth/localAuth.js
+++ src/auth/localAuth.js
@@ -21,10 +21,6 @@ const verifyCustomer = async (email, password, done) => {
 const localStrategy = new LocalStrategy(options, verifyCustomer);
 passport.use(localStrategy);
 
-passport.serializeUser((customer, done) => {
-    done(null, customer.id);
-});
-
 localAuth.authenticate = () => {
     return passport.authenticate('local');
 };
