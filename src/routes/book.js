69dd2e787bfabac069c3a7a9111f80b14019837c
dan7sc <su7cheng@gmail.com>
2020-04-07 14:28:27 -0300

    Change paths for book routes

--- src/routes/book.js
+++ src/routes/book.js
@@ -5,11 +5,11 @@ const router = new Router();
 
 router.route('/')
     .get(BookCtrl.apiGetBooks);
-router.route('/:bookstoreId/books')
+router.route('/bookstore/:bookstoreId')
     .get(BookCtrl.apiGetBooksByBookstoreId)
     .post(BookCtrl.apiAddBook);
-router.route('/:bookstoreId/books/:bookId')
-    .get(BookCtrl.apiGetBookByBookstoreId)
+router.route('/id/:bookId')
+    .get(BookCtrl.apiGetBookById)
     .put(BookCtrl.apiUpdateBook)
     .delete(BookCtrl.apiDeleteBook);
 
