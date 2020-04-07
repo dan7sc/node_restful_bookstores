d35dd0f79aa10125bc3effef8ea4f625a71ba2ae
dan7sc <su7cheng@gmail.com>
2020-04-07 13:26:59 -0300

    Add bookstore/new route

--- src/routes/bookstore.js
+++ src/routes/bookstore.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import BookstoreCtrl from '../controllers/bookstore';
+import jwtAuth from '../auth/jwtAuth';
 
 const router = new Router();
 
 router.route('/')
-    .get(BookstoreCtrl.apiGetBookstores)
+    .get(BookstoreCtrl.apiGetBookstores);
+router.route('/new')
+    .all(jwtAuth.authenticate())
     .post(BookstoreCtrl.apiAddBookstore);
 router.route('/:bookstoreId')
     .get(BookstoreCtrl.apiGetBookstoreById)
+    .all(jwtAuth.authenticate())
     .put(BookstoreCtrl.apiUpdateBookstore)
     .delete(BookstoreCtrl.apiDeleteBookstore);
 
